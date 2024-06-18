13505a3362cc8d49d019987106508559753ecce4
n99joon <n99joon@gmail.com>
2024-06-17 18:37:52 -0700

    Custom Directed_Modularity

--- Codes/Custom_Func.py
+++ Codes/Custom_Func.py
@@ -33,6 +33,35 @@ def modularity(G, communities, weight="weight", resolution=1):
             mod+=1
     return mod
 
+def directed_modularity(G, communities, weight="weight", resolution=1):
+    mod = 0
+    #loop through every community
+    for community in communities:
+        com_sum=0
+        #loop through every pair of nodes in the community
+        if len(community) > 0:
+            for u, v in itertools.combinations(community, 2):
+                #add the weight of the edge between the two nodes
+                if G.has_edge(u,v):
+                    com_sum+= (
+                        G[u][v]['weight']
+                    )
+                if G.has_edge(v,u):
+                    com_sum+= (
+                        G[v][u]['weight']
+                    )
+            for node in community:
+                #add the weight of the self-loop
+                #if there is a self loop
+                if G.has_edge(node,node):
+                    com_sum+= (
+                        G[node][node]['weight']
+                    )
+        if com_sum >=2:
+            mod+=1
+        # print('mod: ',mod)
+    return mod
+  
 def _one_level(G, m, partition, resolution=1, is_directed=False, seed=None):
     """Calculate one level of the Louvain partitions tree
 
@@ -62,8 +91,7 @@ def _one_level(G, m, partition, resolution=1, is_directed=False, seed=None):
     if is_directed:
         in_degrees = dict(G.in_degree(weight="weight"))
         out_degrees = dict(G.out_degree(weight="weight"))
-        Stot_in = list(in_degrees.values())
-        Stot_out = list(out_degrees.values())
+        
         # Calculate weights for both in and out neighbors without considering self-loops
         nbrs = {}
         for u in G:
@@ -91,28 +119,26 @@ def _one_level(G, m, partition, resolution=1, is_directed=False, seed=None):
             if is_directed:
                 in_degree = in_degrees[u]
                 out_degree = out_degrees[u]
-                Stot_in[best_com] -= in_degree
-                Stot_out[best_com] -= out_degree
-                remove_cost = (
-                    -weights2com[best_com] / m
-                    + resolution
-                    * (out_degree * Stot_in[best_com] + in_degree * Stot_out[best_com])
-                    / m**2
-                )
+                
             else:
                 degree = degrees[u]
             for nbr_com, wt in weights2com.items():
                 if is_directed:
+                    #print('neighbor_com: ',nbr_com)
+                    new_partition = copy.deepcopy(inner_partition)
+                    # print('u: ',u)
+                    # print('partition: ',new_partition)
+                    # print('node2com[u]: ',node2com[u])
+                    new_partition[node2com[u]].remove(u)
+                    # print('node2com[u]: ',node2com[u])
+                    new_partition[nbr_com].add(u)
+                    # print('new_partition: ',new_partition)
+                    partition_temp = copy.deepcopy(inner_partition)
                     gain = (
-                        remove_cost
-                        + wt / m
-                        - resolution
-                        * (
-                            out_degree * Stot_in[nbr_com]
-                            + in_degree * Stot_out[nbr_com]
-                        )
-                        / m**2
+                        directed_modularity(G, new_partition, weight="weight", resolution=resolution)
+                        - directed_modularity(G, partition_temp, weight="weight", resolution=resolution)    
                     )
+                    # print('gain: ',gain)
                 else:
                     #print('neighbor_com: ',nbr_com)
                     new_partition = copy.deepcopy(inner_partition)
@@ -137,11 +163,11 @@ def _one_level(G, m, partition, resolution=1, is_directed=False, seed=None):
                 if gain > best_mod:
                     best_mod = gain
                     best_com = nbr_com
-            if is_directed:
-                Stot_in[best_com] += in_degree
-                Stot_out[best_com] += out_degree
-            else:
-                Stot[best_com] += degree
+            # if is_directed:
+            #     # Stot_in[best_com] += in_degree
+            #     # Stot_out[best_com] += out_degree
+            # else:
+            #     Stot[best_com] += degree
             if best_com != node2com[u]:
                 # print('best_com: ',best_com)
                 com = G.nodes[u].get("nodes", {u})
@@ -154,4 +180,5 @@ def _one_level(G, m, partition, resolution=1, is_directed=False, seed=None):
                 node2com[u] = best_com
     partition = list(filter(len, partition))
     inner_partition = list(filter(len, inner_partition))
+    # print('inner_partition: ',inner_partition)
     return partition, inner_partition, improvement
